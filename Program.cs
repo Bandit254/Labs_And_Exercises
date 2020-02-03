1c3a603e6420cc65b31d04ebdda1f28b00c12511
Zachary Silvis <zachary.w.silvis@outlook.com>
2020-02-03 14:12:21 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Labs_And_Exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
