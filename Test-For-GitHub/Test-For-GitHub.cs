5dc5f8c0cbbcad0bd92c638a6abe13a489d88eb4
Zachary Silvis <60155477+Bandit254@users.noreply.github.com>
2020-02-18 21:03:54 -0800

    Catching up on committing projects (18FEB2020)

---
+++ Test-For-GitHub/Test-For-GitHub.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Test_For_GitHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello GitHub! I have successfully synced to your wonderful site!");
+        }
+    }
+}
