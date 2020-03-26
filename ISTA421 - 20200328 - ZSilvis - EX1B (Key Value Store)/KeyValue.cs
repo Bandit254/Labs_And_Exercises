f2622026fa3ac82641fb8c95297e265bed20684b
Zachary Silvis <60155477+Bandit254@users.noreply.github.com>
2020-03-26 16:54:55 -0700

    Added a demo ASP.NET website that shows how to use Entity Framework (data-first approach).

---
+++ ISTA421 - 20200328 - ZSilvis - EX1B (Key Value Store)/KeyValue.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ISTA421___20200328___ZSilvis___EX1B__Key_Value_Store_
+{
+    public struct  KeyValue
+    {
+
+    }
+}
---
+++ ISTA421 - 20200328 - ZSilvis - EX1B (Key Value Store)/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ISTA421___20200328___ZSilvis___EX1B__Key_Value_Store_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ISTA421 - 20200328 - ZSilvis - EX1B - (Key Value Store).\n");
+
+            var d = new MyDictionary();
+            try
+            {
+                Console.WriteLine(d["Cats"]);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            d["Cats"] = 42;
+            d["Dogs"] = 17;
+            Console.WriteLine($"{(int)d["Cats"]}, {(int)d["Dogs"]}");
+        }
+    }
+}
