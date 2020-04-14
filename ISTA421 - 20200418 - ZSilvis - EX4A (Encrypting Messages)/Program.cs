a23bceb637df7d7f09e1f7bea76a8813a0dd8b5e
Zachary Silvis <60155477+Bandit254@users.noreply.github.com>
2020-04-14 11:33:15 -0700

    Added a project that performs 3 simple encryption/decryption algortihms

---
+++ ISTA421 - 20200418 - ZSilvis - EX4A (Encrypting Messages)/Program.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ISTA421___20200418___ZSilvis___EX4A__Encrypting_Messages_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write a program that cleans and encrypts a given string using three different types of encryption keys.\n");
+            Console.Write("Enter a plain text message: ");
+            string input = Console.ReadLine();
+            string cleanedInput = CleanMessage(input);
+            Console.Write("Enter a simple encryption key (a single alphabetical character): ");
+            string simpleKey = Console.ReadLine();
+            Console.Write("Enter a multi-key (more than one alphabetical character, but less than the characters in the plain text message): ");
+            string multiKey = Console.ReadLine();
+            Console.WriteLine($"\n\nThe message you entered was '{input}'");
+            Console.WriteLine($"The simple encryption key you entered was '{simpleKey}'");
+            Console.WriteLine($"The multi-key you entered was '{multiKey}'\n");
+            string encryptedStringSimple = SimpleKeyEncryption(cleanedInput, simpleKey);
+            string encryptedMultiKey = MultiKeyEncryption(cleanedInput, multiKey);
+            string continuousKey = GenerateContinuousKey(cleanedInput, multiKey);
+            string continuousKeyMessage = ContinuousKeyEncryption(cleanedInput, continuousKey);
+            Console.WriteLine($"\nThe encrypted message using the simple key is '{encryptedStringSimple}'");
+            Console.WriteLine($"The encrypted message using the multi-key is: '{encryptedMultiKey}'");
+            Console.WriteLine($"The encrypted message using the continuous key is: '{continuousKeyMessage}.'\n");
+            string decryptedSimple = DecryptSimpleKey(encryptedStringSimple, simpleKey);
+            string decryptedMultiKey = DecryptedMultiKey(encryptedMultiKey, multiKey);
+            string decryptContinuousKey = DecryptContinuousKey(continuousKeyMessage, continuousKey);
+            Console.WriteLine($"\nThe decrypted message using the simple key is '{decryptedSimple}'");
+            Console.WriteLine($"The decrypted message using the multi-key is '{decryptedMultiKey}'");
+            Console.WriteLine($"The decrypted message using the continuous key is '{decryptContinuousKey}'");
+        }
+        public static string CleanMessage(string input)
+        {
+            string cleanedPTMessage = "";
+            input = input.ToUpper();
+            char[] inputCharArray = input.ToCharArray();
+            List<char> cleanedCharList = new List<char>();
+            for (int i = 0; i < inputCharArray.Length; i++)
+            {
+                if (inputCharArray[i]<=90 && inputCharArray[i]>=65)
+                {
+                    cleanedCharList.Add(inputCharArray[i]);
+                }
+            }
+            for (int i = 0; i < cleanedCharList.Count; i++)
+            {
+                cleanedPTMessage += cleanedCharList[i];
+            }
+            return cleanedPTMessage;
+        }
+        public static string SimpleKeyEncryption(string input, string simpleKey)
+        {
+            string encryptedStringSimple = "";
+            simpleKey = simpleKey.ToUpper();
+            int numSpaces = Convert.ToChar(simpleKey) - 64;
+            char[] inputCharArray = input.ToCharArray();
+            for (int i = 0; i < inputCharArray.Length; i++)
+            {
+                char nextChar = Convert.ToChar(inputCharArray[i] + numSpaces);
+                if (nextChar>90)
+                {
+                    nextChar = Convert.ToChar(nextChar - 26);
+                }
+                encryptedStringSimple += nextChar;
+            }
+            return encryptedStringSimple;
+        }
+        public static string DecryptSimpleKey(string encryptedMessage, string simpleKey)
+        {
+            string decryptedSimple = "";
+            simpleKey = simpleKey.ToUpper();
+            int numSpaces = Convert.ToChar(simpleKey) - 64;
+            for (int i = 0; i < encryptedMessage.Length; i++)
+            {
+                char nextChar = Convert.ToChar(encryptedMessage[i] - numSpaces);
+                if (nextChar < 65)
+                {
+                    nextChar = Convert.ToChar(nextChar + 26);
+                }
+                decryptedSimple += nextChar;
+            }
+            return decryptedSimple;
+        }
+        public static string MultiKeyEncryption(string input, string multiKey)
+        {
+            string encryptedMultiKeyString = "";
+            multiKey = multiKey.ToUpper();
+            if (multiKey.Length>=input.Length)
+            {
+                for (int i = 0; i < input.Length; i++)
+                {
+                    int numSpaces = Convert.ToChar(multiKey[i]) - 64;
+                    char nextChar = Convert.ToChar(input[i] + numSpaces);
+                    if (nextChar > 90)
+                    {
+                        nextChar = Convert.ToChar(nextChar - 26);
+                    }
+                    encryptedMultiKeyString += nextChar;
+                }
+            }
+            for (int i = 0; i < input.Length; i++)
+            {
+                int numSpaces = Convert.ToChar(multiKey[i - ((i / multiKey.Length) * multiKey.Length)]) - 64;
+                char nextChar = Convert.ToChar(input[i] + numSpaces);
+                if (nextChar > 90)
+                {
+                    nextChar = Convert.ToChar(nextChar - 26);
+                }
+                encryptedMultiKeyString += nextChar;
+            }
+            return encryptedMultiKeyString;
+        }
+        public static string DecryptedMultiKey(string encryptedMultiKeyMessage, string multiKey)
+        {
+            string decryptedMultiKeyMessage = "";
+            multiKey = multiKey.ToUpper();
+            if (multiKey.Length >= encryptedMultiKeyMessage.Length)
+            {
+                for (int i = 0; i < encryptedMultiKeyMessage.Length; i++)
+                {
+                    int numSpaces = Convert.ToChar(multiKey[i]) - 64;
+                    char nextChar = Convert.ToChar(encryptedMultiKeyMessage[i] - numSpaces);
+                    if (nextChar < 65)
+                    {
+                        nextChar = Convert.ToChar(nextChar + 26);
+                    }
+                    decryptedMultiKeyMessage += nextChar;
+                }
+            }
+            for (int i = 0; i < encryptedMultiKeyMessage.Length; i++)
+            {
+                int numSpaces = Convert.ToChar(multiKey[i - ((i / multiKey.Length) * multiKey.Length)]) - 64;
+                char nextChar = Convert.ToChar(encryptedMultiKeyMessage[i] - numSpaces);
+                if (nextChar < 65)
+                {
+                    nextChar = Convert.ToChar(nextChar + 26);
+                }
+                decryptedMultiKeyMessage += nextChar;
+            }
+            return decryptedMultiKeyMessage;
+        }
+        public static string GenerateContinuousKey(string input, string multiKey)
+        {
+            string continuousKey = "";
+            if (multiKey.Length > input.Length)
+            {
+                Console.WriteLine($"Error: Key length must be shorter than the message length. You must provide a key with a length less than {input.Length} characters long.\n");
+                Console.Write("Enter a new key: ");
+                multiKey = Console.ReadLine();
+            }
+            multiKey = multiKey.ToUpper();
+            for (int i = 0; i < multiKey.Length; i++)
+            {
+                continuousKey += multiKey[i];
+            }
+            for (int i = 0; i < (input.Length - multiKey.Length); i++)
+            {
+                continuousKey += input[i];
+            }
+
+            return continuousKey;
+        }
+        public static string ContinuousKeyEncryption(string input, string continuousKey)
+        {
+            if (continuousKey.Length != input.Length)
+            {
+                return "Error - key and message length do not match";
+            }
+            string encryptedContinuousMessage = "";
+            for (int i = 0; i < input.Length; i++)
+            {
+                int numSpaces = Convert.ToChar(continuousKey[i]) - 64;
+                char nextChar = Convert.ToChar(input[i] + numSpaces);
+                if (nextChar > 90)
+                {
+                    nextChar = Convert.ToChar(nextChar - 26);
+                }
+                encryptedContinuousMessage += nextChar;
+            }
+            return encryptedContinuousMessage;
+        }
+        public static string DecryptContinuousKey(string encryptedContinuousMessage, string continuousKey)
+        {
+            string decryptedContinuousMessage = "";
+            for (int i = 0; i < encryptedContinuousMessage.Length; i++)
+            {
+                int numSpaces = Convert.ToChar(continuousKey[i]) - 64;
+                char nextChar = Convert.ToChar(encryptedContinuousMessage[i] - numSpaces);
+                if (nextChar < 65)
+                {
+                    nextChar = Convert.ToChar(nextChar + 26);
+                }
+                decryptedContinuousMessage += nextChar;
+            }
+            return decryptedContinuousMessage;
+        }
+    }
+}
